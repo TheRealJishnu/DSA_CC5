547954307abe5c0fb9617d08aea429a82243721f
TheRealJishnu <93379644+TheRealJishnu@users.noreply.github.com>
2022-09-07 16:36:47 +0530

    Update CircQueue.c

--- Queue/CircQueue.c
+++ Queue/CircQueue.c
@@ -5,29 +5,82 @@ int front = -1;
 int back = -1;
 int *q;
 int size;
-//int counter = 1;
 
-void EnQueue() {
-    printf("Enter Data to be Inserted: ");
-    int data;
-    scanf("%d", &data);
-    if(back == size-1) {
-        q[0] = data;
-        front = 0;
+
+int isFull(){
+    if ((back + 1) % size == front) {
+        return 1;
     }
     else {
-        q[++front] = data;
+        return 0;
     }
-
 }
 
-void DeQueue() {
-    if (front == back){
-        printf("Queue is Empty");
+void EnQueue() {
+
+    if (isFull()) {
+        printf("Queue is Full :( Dequeue to Make Room For More Space\n");
         return;
     }
     else {
-        printf("Item %d is out of the Queue", q[front]);
-        
+        back = (back + 1) % size;
+        printf("Enter Data to Be Added In Queue :    ");
+        scanf("%d", &q[back]);
+
+        //In Case of First Element Insertion
+        if(back == 0 && front == -1) {
+            front++;
+        }
+    }
+}
+
+void DeQueue() {
+    if (front == -1) {
+        printf("Queue is Empty :(   Please Enqueue to Dequeue\n");
+        return;
+    }
+    else {
+        printf("Data %d Out Of Queue\n", q[front]);
+        if (front == back) {                        //If only One data in Queue
+            front = -1;
+            back = -1;
+        }
+        else {
+            front = (front + 1) % size;
+        }
+
+    }
+}
+
+void Display() {
+    int i = front;
+    printf("Queue: ");
+    do {
+        printf("%d\t", q[front]);
+        i = (i + 1) % size;
+
+    }while (i != ((front + 1) % size));
+}
+
+int main() {
+    printf("Enter Size of Queue : ");
+    scanf("%d", &size);
+    q = (int*)malloc(size * sizeof(int));
+    char ch = 'y';
+    while (ch == 'y') {
+        printf("Enter 1 to Enqueue\n");
+        printf("Enter 2 to Dequeue\n");
+        printf("Enter 3 to Display\n");
+        int choice;
+        scanf("%d", &choice);
+        switch(choice) {
+            case 1 : EnQueue(); break;
+            case 2 : DeQueue(); break;
+            case 3 : Display(); break;
+            default : printf("Please Enter A Valid Choice\n"); break;
+        }
+        printf("Do You Want to Keep Going? ");
+        scanf("%c", &ch);
+        scanf("%c", &ch);
     }
 }
