0f3938bfc9d80daf45e798b5d13d0c67c88082d0
TheRealJishnu <93379644+TheRealJishnu@users.noreply.github.com>
2022-11-02 21:07:30 +0530

    Update BinSearchTree.c

--- Graphs and Trees/BinSearchTree.c
+++ Graphs and Trees/BinSearchTree.c
@@ -13,18 +13,18 @@ node *root = NULL;
 int Insert(int data, node *current)
 {
     //node *temp = current;
-    if(temp == NULL)                        //If Tree is EMPTY
+    if(current == NULL)                        //If Tree is EMPTY
     {
         current = (node*)malloc(sizeof(node));
         current->data = data;
     }
     else                                    //Normal Process Starts
     {
-        if(data < temp->data)
+        if(data < current->data)
         {
             return Insert(data, current->left);
         }
-        else if(data < temp->data)
+        else if(data < current->data)
         {
             return Insert(data, current->right);
         }
@@ -35,12 +35,70 @@ int Insert(int data, node *current)
         }
     }
 }
+
+int SearchValue(int key, node *current)
+{
+    if(key == current->data)
+    {
+        return 1;
+    }
+    else if(key > current->data)
+    {
+        if(current->right != NULL)
+            return SearchValue(key, current->right);
+        else
+            return 0;
+    }
+    else if(key < current->data)
+    {
+        if(current->left != NULL)
+            return SearchValue(key, current->left);
+        else
+            return 0;
+    }
+}
 int main()
 {
-    node *root = NULL;
-    switch(choice)
+    //node *root = NULL;
+    char ch = 'y';
+    while(ch == 'y')
     {
-        case 1 : printf
+        printf("\nWELCOME TO BINARY SEARCH TREE\n");
+        printf("TYPE 1 TO INSERT DATA\nTYPE 2 TO SEARCH DATA\n");
+        int choice;
+        scanf("%d", &choice);
+        switch(choice)
+        {
+            case 1 :
+                {
+                    printf("Enter Data : ");
+                    int data;
+                    scanf("%d", &data);
+                    Insert(data, root);
+                    printf("%d added SUCCESSFULLY\n");
+                    break;
+                }
+            case 2 :
+                {
+                    printf("Enter Data To Search: ");
+                    int data;
+                    scanf("%d", &data);
+                    if(SearchValue(data, root))
+                        printf("Data Found\n");
+                    else
+                        printf("Data NOT Found\n");
+                    break;
+                }
+            default :
+                {
+                    printf("INVALID CHOICE\n");
+                    break;
+                }
+        }
+        printf("Do You Want To Continue?(y/n)    ");
+        scanf("%c", &ch);
+        scanf("%c", &ch);
     }
+
     return 0;
 }
